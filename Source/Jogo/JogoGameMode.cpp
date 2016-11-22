41c5ddcf965ddbd4686ced5c3d466993f048d53d
tsgarcia <thamires.luz@outlook.com>
2016-11-22 13:13:09 -0200

    Ajustes Game Mode

--- Source/Jogo/JogoGameMode.cpp
+++ Source/Jogo/JogoGameMode.cpp
@@ -19,24 +19,3 @@ AJogoGameMode::AJogoGameMode() {
 	HUDClass = ADoctorHUD::StaticClass();
 
 }
-
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Jogo.h"
-#include "JogoGameMode.h"
-#include "MyPlayerController.h"
-#include "Doctor.h"
-#include "Cyborg.h"
-#include "CyborgHUD.h"
-#include "DoctorHUD.h"
-
-AJogoGameMode::AJogoGameMode() {
-	PlayerControllerClass = AMyPlayerController::StaticClass();
-
-	DefaultPawnClass = ACyborg::StaticClass();
-	HUDClass = ACyborgHUD::StaticClass();
-
-//	DefaultPawnClass = ADoctor::StaticClass();
-//	HUDClass = ADoctorHUD::StaticClass();
-
-}
